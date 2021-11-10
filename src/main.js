69e4d31d8dd67a2ca54af5429f7c1f2273f9160e
Bolas <ltzuen@gmail.com>
2021-11-10 13:07:52 +0800

    引入bootstrap

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 const vue = createApp(App)
 vue.use(VueAxios, axios)
